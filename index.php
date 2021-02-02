5e18154cf30c1c17092aceafc649626ca702f982
AndreaG <gaggiandrea@gmail.com>
2021-02-02 16:10:38 +0100

    php solution ok. style to be added

--- index.php
+++ index.php
@@ -1,33 +1,44 @@
+<?php
+include './src/php_partials/albums.php'
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>php-ajax-dischi</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap"
-            rel="stylesheet"
-        />
-        <link rel="stylesheet" href="./dist/assets/index.css" />
-    </head>
-    <body>
-        <nav>
-            <div class="container">
-                <img src="./dist/assets/img/spotify-2.svg" alt="" height="40" />
-            </div>
-        </nav>
 
-        <main>
-            <div class="container">
-                <div class="albums">
-                    <div class="album"></div>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>php-ajax-dischi</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="./dist/assets/index.css" />
+</head>
+
+<body>
+    <nav>
+        <div class="container">
+            <img src="./dist/assets/img/spotify-2.svg" alt="" height="40" />
+        </div>
+    </nav>
+
+    <main>
+        <div class="container">
+            <div class="albums">
+                <div class="album">
+                    <?php foreach ($albums as $album => $details) {?>
+                    <img src="<?php echo $details["poster"]; ?>" class="poster">
+                    <h2 class="title"><?php echo $details["title"]; ?></h2>
+                    <h3 class="author"><?php echo $details["author"]; ?></h3>
+                    <h4 class="year"><?php echo $details["year"]; ?></h4>
+                    <?php }?>
                 </div>
             </div>
-        </main>
-        <!-- scripts -->
-        <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-        <script src="./dist/assets/index.js"></script>
-    </body>
+        </div>
+    </main>
+    <!-- scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="./dist/assets/index.js"></script>
+</body>
+
 </html>
--- src/php_partials/albums.php
+++ src/php_partials/albums.php
@@ -1,6 +1,30 @@
 <?php
 // $albums = [];
 
-$prova_api = file_get_contents("https://flynn.boolean.careers/exercises/api/array/music");
+#mia soluzione
+$raw = file_get_contents("https://flynn.boolean.careers/exercises/api/array/music");
+$decoded = json_decode($raw, true);
+$albums = $decoded["response"];
+//var_dump($albums);
 
-var_dump($prova_api);
+/*
+PRECOMPILATO DA POSTMAN
+ */
+// $curl = curl_init();
+// curl_setopt_array($curl, array(
+//     CURLOPT_URL => 'https://flynn.boolean.careers/exercises/api/array/music',
+//     CURLOPT_RETURNTRANSFER => true,
+//     CURLOPT_ENCODING => '',
+//     CURLOPT_MAXREDIRS => 10,
+//     CURLOPT_TIMEOUT => 0,
+//     CURLOPT_FOLLOWLOCATION => true,
+//     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//     CURLOPT_CUSTOMREQUEST => 'GET',
+//     CURLOPT_HTTPHEADER => array(
+//         'Cookie: XSRF-TOKEN=eyJpdiI6IllrYXY1Q3pRZ21PK1FCMkdUMk5zVnc9PSIsInZhbHVlIjoiMXRrRm4rOEFURk1OQ2YzN2lCM1B0eWhTRHhZYVRHVjNNZW1Ld09ZOGpXSjRCMENlYXVDUjhyT1F0V3RsMmdUeCIsIm1hYyI6IjY5M2I4YjBiNzI5ZDE3YmRiMjBkZmRhODFhYjZlMzI4ZGZlMzI0NThhYWE1ZGE0MzYxMjNjYzFhMDg2NjBkZDUifQ%3D%3D; laravel_session=eyJpdiI6IkVrbUNSOFZ3VWdsUGExOGFZV21jb2c9PSIsInZhbHVlIjoiZVk5S1NtMVlqMUd4c0FCc3lZWkFqUGw1NWo4blR1d25UOThkcTNGZHVEYys3TTFjcHB2WEluV25wOTlzWndMSSIsIm1hYyI6IjQ0ZTEzMTAwYjg1ZTgwYjA1ZmQ4NTYxMGU3NDcxMmY4ZWJiMTQ0ZGM2ZGQzNDYzNzNkZjZiYzg2NTBiMzY3M2UifQ%3D%3D',
+//     ),
+// ));
+// $response = curl_exec($curl);
+// curl_close($curl);
+// $albums = json_decode($response, true);
+// var_dump($albums);
