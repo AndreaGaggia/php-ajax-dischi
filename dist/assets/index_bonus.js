cb05088465baa3915254c9e4ebe7387cb82deb2e
AndreaG <gaggiandrea@gmail.com>
2021-02-03 12:54:33 +0100

    bonus done in js vanilla

--- dist/assets/index_bonus.js
+++ dist/assets/index_bonus.js
@@ -3,10 +3,36 @@
   !*** ./src/index_bonus.js ***!
   \****************************/
 axios.get("./src/php_partials/albs_for_js.php").then(function (response) {
-  var data = response.data;
-  var albums = document.querySelector(".albums");
+  var data = response.data; //la select per filtrare gli album in base all'artista
+
+  var select = document.getElementById("filter-select");
+  data.forEach(function (element) {
+    select.insertAdjacentHTML("beforeend", "<option value=\"".concat(element.author, "\">").concat(element.author, "</option>"));
+  });
+  generateHTML(data); //filtro la selezione in base al valore della select
+
+  var filtered = [];
+  select.addEventListener("change", function () {
+    var option = select.value;
+
+    if (option != "All") {
+      var _filtered = data.filter(function (element) {
+        return element.author == option;
+      });
+
+      generateHTML(_filtered);
+    } else {
+      generateHTML(data);
+    }
+
+    console.log(option);
+  });
+}); //funzione per generare contenuti nel DOM
+
+var generateHTML = function generateHTML(arr) {
+  //a partire dai data creo un array di oggetti con il markup per ogni singolo album
   var markup = [];
-  data.forEach(function (album) {
+  arr.forEach(function (album) {
     var element = {
       poster: "<img src=\"".concat(album.poster, "\" />"),
       title: "<h2 class=\"title\">".concat(album.title, "</h2>"),
@@ -14,7 +40,10 @@ axios.get("./src/php_partials/albs_for_js.php").then(function (response) {
       year: "<h3 class=\"author\">".concat(album.year, "</h3>")
     };
     markup.push(element);
-  });
+  }); //inserisco gli oggetti con il markup nel documento
+
+  var albums = document.querySelector(".albums");
+  albums.innerHTML = "";
   markup.forEach(function (element) {
     var album = document.createElement("div");
     album.classList.add("album");
@@ -25,6 +54,6 @@ axios.get("./src/php_partials/albs_for_js.php").then(function (response) {
 
     albums.insertAdjacentElement("beforeend", album);
   });
-});
+};
 /******/ })()
 ;
--- src/index_bonus.js
+++ src/index_bonus.js
@@ -1,8 +1,36 @@
 axios.get("./src/php_partials/albs_for_js.php").then((response) => {
-    const data = response.data;
-    const albums = document.querySelector(".albums");
+    let data = response.data;
+
+    //la select per filtrare gli album in base all'artista
+    const select = document.getElementById("filter-select");
+    data.forEach((element) => {
+        select.insertAdjacentHTML(
+            "beforeend",
+            `<option value="${element.author}">${element.author}</option>`
+        );
+    });
+
+    generateHTML(data);
+
+    //filtro la selezione in base al valore della select
+    let filtered = [];
+    select.addEventListener("change", () => {
+        let option = select.value;
+        if (option != "All") {
+            let filtered = data.filter((element) => element.author == option);
+            generateHTML(filtered);
+        } else {
+            generateHTML(data);
+        }
+        console.log(option);
+    });
+});
+
+//funzione per generare contenuti nel DOM
+const generateHTML = (arr) => {
+    //a partire dai data creo un array di oggetti con il markup per ogni singolo album
     const markup = [];
-    data.forEach((album) => {
+    arr.forEach((album) => {
         let element = {
             poster: `<img src="${album.poster}" />`,
             title: `<h2 class="title">${album.title}</h2>`,
@@ -11,6 +39,10 @@ axios.get("./src/php_partials/albs_for_js.php").then((response) => {
         };
         markup.push(element);
     });
+
+    //inserisco gli oggetti con il markup nel documento
+    const albums = document.querySelector(".albums");
+    albums.innerHTML = "";
     markup.forEach((element) => {
         const album = document.createElement("div");
         album.classList.add("album");
@@ -19,4 +51,4 @@ axios.get("./src/php_partials/albs_for_js.php").then((response) => {
         }
         albums.insertAdjacentElement("beforeend", album);
     });
-});
+};
